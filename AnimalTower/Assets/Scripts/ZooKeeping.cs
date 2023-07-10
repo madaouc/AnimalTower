b0ffa949421dd1591f4d2c0d43675331c5b9b5b1
Macaroni <madaouc@gmail.com>
2023-07-10 20:22:32 +0800

    ZooKeeping

---
+++ AnimalTower/Assets/Scripts/ZooKeeping.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZooKeeping : MonoBehaviour
+{
+    public GameObject []animals;
+    GameObject currentAnimal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixUpdate()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            int randNum = Random.Range(0, 3);
+            Debug.Log(randNum);
+            //Vector3 spawnPos = transform.position;
+            //Quaternion spawnRot = transform.rotation;
+
+
+            currentAnimal =  Instantiate(animals[randNum]);
+            currentAnimal.transform.position = transform.position;
+        }
+    }
+}
