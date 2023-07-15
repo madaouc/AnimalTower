12e8760affc720fdbdb660402d2baed330310dfa
Macaroni <madaouc@gmail.com>
2023-07-15 11:05:53 +0800

    Animal Rotation

--- AnimalTower/Assets/Scripts/ZooKeeper.cs
+++ AnimalTower/Assets/Scripts/ZooKeeper.cs
@@ -7,7 +7,13 @@ public class ZooKeeper : MonoBehaviour
     public GameObject [] animals;
     GameObject currentAnimal;
 
+    [Header("Zoo Movement")]
     public float movement = 0.2f;
+    public int turns = 8;
+    int turnCount = 0;
+
+    public float gravity = 0.5f;
+
 
     bool haveAnimal = false;
 
@@ -27,7 +33,6 @@ public class ZooKeeper : MonoBehaviour
         // x = x + 1
         if(Input.GetKey(KeyCode.A))
         {
-            
             Debug.Log("A");
             zooPos.x = zooPos.x - movement * Time.deltaTime;
         }
@@ -48,15 +53,44 @@ public class ZooKeeper : MonoBehaviour
             int index = Random.Range(0, animals.Length);
             currentAnimal = Instantiate(animals[index]);
             currentAnimal.transform.position = transform.position;
+            turnCount = 0;
         }
 
         //if (Input.GetKeyDown(KeyCode.Space) && haveAnimal == true)
         if (Input.GetKeyDown(KeyCode.Space) && haveAnimal)
         {
-            currentAnimal.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+            currentAnimal.GetComponent<Rigidbody2D>().gravityScale =
+                gravity;
             haveAnimal = false;
-        }   
+        }
+      
+    //animal follow    
+        if(haveAnimal)
+        {
+            currentAnimal.transform.position = transform.position;
+        }
 
+    //animal Rotation
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            //turnCount = turnCount + 1;
+            turnCount -= 1;
+        }
+        else if(Input.GetKeyDown(KeyCode.Q))
+        {
+            turnCount += 1;
+        }
+
+        float zAngle = (360.0f / turns) * turnCount;
+        Debug.Log("zAngle: " + zAngle);
+
+        Vector3 animalRot = new Vector3(0.0f, 0.0f, zAngle);
+        
+        if(haveAnimal)
+        {
+            currentAnimal.transform.eulerAngles =
+                animalRot;
+        }
 
 
     }
