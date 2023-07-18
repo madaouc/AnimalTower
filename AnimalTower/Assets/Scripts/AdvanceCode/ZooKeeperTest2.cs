3157db5aaf5d642101bbaafc73c0a5eded6c6a75
Macaroni <madaouc@gmail.com>
2023-07-19 04:06:21 +0800

    functions updated
    Auto Gen, SE, BGM, UI buttons

--- AnimalTower/Assets/Scripts/AdvanceCode/ZooKeeperTest2.cs
+++ AnimalTower/Assets/Scripts/AdvanceCode/ZooKeeperTest2.cs
@@ -2,64 +2,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZooKeeperTest : MonoBehaviour
+public class ZooKeeperTest2 : MonoBehaviour
 {
     public GameObject [] animals;
     GameObject currentAnimal;
-    
-    public float targetSpawnTime = 1.0f;
-    float timeCounter = 0.0f;
-    bool counterFinish = true;
 
     [Header("Zoo Movement")]
     public float movement = 0.5f;
     public int turns = 8;
     int turnCount = 0;
-
     public float gravity = 0.5f;
 
     bool haveAnimal = false;
+    bool letGo = false;
+
+    bool gameOverFlag = false;
+
+
+    AudioSource soundFx;
+    [Header("SFX")]
+    public AudioClip clipLetGo, clipGameOver;
+    
     
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
+        soundFx = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!haveAnimal && !counterFinish)
-        {
-            timeCounter += Time.deltaTime;
-        }
-        if(timeCounter > targetSpawnTime)
-        {
-            timeCounter = 0.0f;
-            counterFinish = true;
-        }
+        if (gameOverFlag)
+            return;
+
 
         //spawn animal
-        if (counterFinish && !haveAnimal)
+        if (!haveAnimal)
         {
             haveAnimal = true;
+            letGo = false;
+            turnCount = 0;
+
             int index = Random.Range(0, animals.Length);
             currentAnimal = Instantiate(animals[index]);
             currentAnimal.transform.position = transform.position;
-            turnCount = 0;
         }
+    
 
         //if (Input.GetKeyDown(KeyCode.Space) && haveAnimal == true)
         if (Input.GetKeyDown(KeyCode.Space) && haveAnimal)
         {
             currentAnimal.GetComponent<Rigidbody2D>().gravityScale =
                 gravity;
-            haveAnimal = false;
-            counterFinish = false;
+            letGo = true;
+            soundFx.clip = clipLetGo;
+            soundFx.Play();
         }
 
-     
+        if(haveAnimal)
+        {
+            if (currentAnimal == null)
+            {
+                Debug.Log("GameOver");
+                gameOverFlag = true;
+                GetComponent<SpriteRenderer>().enabled = false;
+                haveAnimal = false;
+                letGo = false;
+
+                soundFx.clip = clipGameOver;
+                soundFx.Play();
+                return;
+            }
+
+            bool landed;
+            landed = currentAnimal.GetComponent<AniLanding>().landed;
+
+            float animalVelocitySqr;
+            animalVelocitySqr = currentAnimal.GetComponent<Rigidbody2D>().velocity.sqrMagnitude;
+
+            if (landed && animalVelocitySqr < 0.01f)
+                haveAnimal = false;
+
+        }
+       
+
 
 
     //zoo Move
@@ -82,8 +111,11 @@ public class ZooKeeperTest : MonoBehaviour
         
     }
 
-    void move(int dir)
+    public void move(int dir)
     {
+        if (letGo)
+            return;
+
         //dir: -1, left; 1, right
         Vector3 zooPos = transform.position;
         zooPos.x = zooPos.x + movement * Time.deltaTime * dir;    
@@ -96,8 +128,11 @@ public class ZooKeeperTest : MonoBehaviour
     }
 
  
-    void rotate(int dir = -1)
+    public void rotate(int dir = -1)
     {
+        if (letGo)
+            return;
+
         //dir: -1, clockwise
         turnCount += dir;
       
